0c84c391a784e414073d6360822349a6323040af
Pedro-Esteves-Al <pedroestevesa@gmail.com>
2024-02-22 11:01:04 -0300

    Primeiro commit

---
+++ scfcapi/src/main/java/pedro/joao/scfcapi/ScfcapiApplication.java
@@ -0,0 +1,13 @@
+package pedro.joao.scfcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScfcapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScfcapiApplication.class, args);
+	}
+
+}
---
+++ scfcapi/src/test/java/pedro/joao/scfcapi/ScfcapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pedro.joao.scfcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScfcapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
